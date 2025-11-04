2b07dfbb1a5c8bf15d0c94c2a532198fc436ffb5
TyreeGreen-hub <tgreen016@student.butte.edu>
2025-11-04 19:39:25 +0000

    Add frontend authentication with React Hook Form

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,15 +1,35 @@
-import { Size } from './size'
-import { Variant } from './variant'
+import React, { forwardRef } from 'react'
+import { getInputSizeStyles, Size } from './size'
 import { HTMLInputTypeAttribute } from 'react'
+import { getVariantBorderStyles, getVariantInputTextStyles, getVariantOutlineStyles, Variant } from './variant'
+import { getCommonInputStyles } from './tokens'
 
-interface InputProps {
+export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
   variant?: Variant
   size?: Size
-  placeholder?: string
-  type?: HTMLInputTypeAttribute
-  value?: any
   setValue?: (newValue: any) => void
-  defaultValue?: any
-  name: string
-  id: string
 }
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ variant = Variant.PRIMARY, size = Size.MEDIUM, setValue, onChange, className, ...props }, ref) => {
+    const sizeCssClasses = getInputSizeStyles(size)
+    const variantOutlineCssClasses = getVariantOutlineStyles(variant)
+    const variantBorderCssClasses = getVariantBorderStyles(variant)
+    const variantInputTextCssClasses = getVariantInputTextStyles(variant)
+    const commonCssClasses = getCommonInputStyles()
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (setValue) setValue(e.target.value)
+      if (onChange) onChange(e)
+    }
+
+    return (
+      <input
+        ref={ref}
+        className={`${sizeCssClasses} ${variantBorderCssClasses} ${variantInputTextCssClasses} ${variantOutlineCssClasses} ${commonCssClasses} ${className ?? ''}`}
+        onChange={handleChange}
+        {...props}
+      />
+    )
+  },
+)
